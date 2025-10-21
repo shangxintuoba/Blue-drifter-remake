be3f5b58e50c2617dad8e6cb9cee489d3514b2a5
伤心拖把 <yiwensimon.ding@gmail.com>
2025-10-21 01:09:57 +0100

    try to draw bullet trail with line renderer, fail

--- Assets/script/Gun.cs
+++ Assets/script/Gun.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -7,25 +9,49 @@ public class Gun : MonoBehaviour
     public Transform Casepoint;
 
     public float MaxDistance = 20f;
-    public LayerMask Mask;
+    public LayerMask ShootableObjects;
+    public LineRenderer BulletTrail;
 
+
+    private void Start()
+    {
+        //BulletTrail.enabled = false;
+    }
+
+    public void Update()
+    {
+        Fire();
+    }
 
     public void Fire()
     {
-        RaycastHit hitInfo;
-        bool hit = Physics.Raycast(Firepoint.position, Firepoint.forward, out hitInfo, MaxDistance, Mask);
-        if (hit)
+        if (Input.GetMouseButton(0))
         {
+            RaycastHit hitInfo;
+            bool hit = Physics.Raycast(Firepoint.position, Firepoint.forward, out hitInfo, MaxDistance, ShootableObjects);
+            if (hit)
+            {
+                DrawBulletTrail(hitInfo.point);
+            }
+            else
+            {
+                DrawBulletTrail(Firepoint.position + Firepoint.forward * MaxDistance);
+
+            }
+            Instantiate(Bulletcaseprefab, Casepoint.position, Quaternion.identity);
+            //BulletTrail.enabled = true;
 
         }
         else
         {
-
-
+            //BulletTrail.enabled = false;
         }
+    }
 
-
-        Instantiate(Bulletcaseprefab, Casepoint.position, Quaternion.identity);
+    private void DrawBulletTrail(Vector3 hitpoint)
+    {
+        BulletTrail.SetPosition(0, hitpoint - Firepoint.forward * 20f);
+        BulletTrail.SetPosition(1, hitpoint);
     }
 
 }
