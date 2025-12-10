962a077b885dcc6d67add76bfbda99d3ca5a87cc
伤心拖把 <yiwensimon.ding@gmail.com>
2025-12-10 22:10:20 +0000

    working with probuilder and enemy

---
+++ Assets/script/Enemy.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject Player;
+
+
+
+
+
+    private void Wander()
+    {
+
+
+    }
+
+    private void Shoot()
+    {
+
+    }
+
+
+    private void Chase()
+    {
+
+
+    }
+}
