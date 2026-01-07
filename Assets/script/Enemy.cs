33fe327dd2e7c98448426f158922e35d4380d32f
伤心拖把 <yiwensimon.ding@gmail.com>
2026-01-07 20:04:29 +0000

    Enemy AI

--- Assets/script/Enemy.cs
+++ Assets/script/Enemy.cs
@@ -5,13 +5,41 @@ public class Enemy : MonoBehaviour
 
     public GameObject Player;
 
+    public enum State
+    {
+        Wandering,
+        Chasing,
+        Shooting
+    }
+
+    public State EnemyState = State.Wandering;
+
+
+    public void Update()
+    {
+        switch (EnemyState)
+        {
+            case State.Wandering:
+                Wander();
+                break;
+
+            case State.Chasing:
+                Chase();
+                break;
+
+            case State.Shooting:
+                Shoot();
+                break;
+        }
+
+    }
 
 
 
 
     private void Wander()
     {
-
+       
 
     }
 
